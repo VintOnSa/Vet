e63b8382144bd23d319b0fc08bda64ac59e1c5c1
Vintonsa <vintonsa@gmail.com>
2026-08-17 02:35:03 -0400

    Sistema Listo

--- Veterinaria/Apps/General/admin.py
+++ Veterinaria/Apps/General/admin.py
@@ -4,8 +4,12 @@ from Apps.General.models import *
 # Register your models here.
 admin.site.register(Bodega)
 admin.site.register(Insumo)
+admin.site.register(Especie)
+admin.site.register(Raza)
 admin.site.register(Personal)
 admin.site.register(Dueno)
 admin.site.register(Paciente)
 admin.site.register(VacunasPaciente)
+admin.site.register(CirugiasPaciente)
+admin.site.register(Procedimiento)
 admin.site.register(Agenda)
--- Veterinaria/Apps/General/models.py
+++ Veterinaria/Apps/General/models.py
@@ -26,6 +26,28 @@ class Insumo(models.Model):
         txt = "Codigo: {0} - Nombre: {1} - Tipo: {2} - Unidad: {3} - Stock: {4} - Precio {5} - Desc: {6} - Ubicacion: {7}"
         return txt.format(self.codigo, self.nombre, self.tipo, self.unidad, self.stock, self.precio, self.descuento, self.ubicacion.nombre)
 
+class Procedimiento(models.Model):
+    nombre = models.CharField(max_length=200, blank=True, null=True)
+    precio = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        txt = "Nombre: {0} - Precio: {1}"
+        return txt.format(self.nombre, self.precio)
+
+class Especie(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre or ''
+
+class Raza(models.Model):
+    especie = models.ForeignKey(Especie, on_delete=models.CASCADE, related_name='razas')
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        txt = "Raza: {0} - Especie: {1}"
+        return txt.format(self.nombre, self.especie.nombre)
+
 class Personal(models.Model):
     nombre = models.CharField(max_length=200, blank=True, null=True)
     rut = models.CharField(max_length=20, blank=True, null=True)
@@ -83,8 +105,29 @@ class VacunasPaciente(models.Model):
         return txt.format(self.paciente.nombre, self.nombre)
 
 
+class CirugiasPaciente(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='cirugias')
+    procedimiento = models.ForeignKey(Procedimiento, on_delete=models.SET_DEFAULT, default=1)
+    nombre = models.CharField(max_length=300, null=True, blank=True)
+    fecha = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        txt = "Paciente: {0} - Procedimiento: {1} - Fecha: {2}"
+        return txt.format(self.paciente.nombre, self.nombre, self.fecha)
+
+
+TIPO_AGENDA_CHOICES = [
+    ('Atención Médica', 'Atención Médica'),
+    ('Hospital', 'Hospital'),
+    ('Quirúrgica', 'Quirúrgica'),
+    ('Terreno', 'Terreno'),
+    ('Tratamiento', 'Tratamiento'),
+]
+
+
 class Agenda(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, null=True, blank=True)
+    tipo = models.CharField(max_length=50, blank=True, null=True, choices=TIPO_AGENDA_CHOICES)
     procedimiento = models.CharField(max_length=200, blank=True, null=True)
     datos_ing = models.CharField(max_length=200, blank=True, null=True)
     fecha = models.DateField(null=True, blank=True)
